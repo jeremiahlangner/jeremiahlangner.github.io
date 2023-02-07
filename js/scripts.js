2626889a115177030f009f91e9b67a7da62b3585
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-02-06 20:27:58 -0700

    update resume

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,42 @@
 /*!
 * Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2022 Start Bootstrap
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-(function(){
-    let stop = {};
+(() => {
+  let stop = {};
 
-    const downArrow = document.querySelector('.down_arrow');
-    document.addEventListener('click', e => {
-        if (e.srcElement && e.srcElement == downArrow) {
-            try {
-                scrollNext();
-            } catch (e) {
-                if (e !== stop) throw e;
-            }
-        }
+  const downArrow = document.querySelector('.down_arrow');
+  const downloadButton = document.getElementById('download');
+
+  document.addEventListener('click', e => {
+    if (e.target && e.target == downloadButton) {
+      window.print();
+    }
+    if (e.target && e.target == downArrow) {
+      try {
+        scrollNext();
+      } catch (e) {
+        if (e !== stop) throw e;
+      }
+    }
+  });
+
+  function scrollNext() {
+    const sections = document.querySelectorAll('.show-on-scroll');
+    [].forEach.call(sections, (section) => {
+      if (offset(section) > window.scrollY + window.innerHeight / 2) {
+        window.scrollTo(0, offset(section) - window.innerHeight / 3);
+        throw stop;
+      }
     });
-    
-    function scrollNext() {
-        const sections = document.querySelectorAll('.show-on-scroll');
-        [].forEach.call(sections, (section) => {         
-            if (offset(section) > window.scrollY + window.innerHeight / 2) {
-                window.scrollTo(0, offset(section) - window.innerHeight / 3 );
-                throw stop;
-            }
-        });
-        window.scrollTo(0, 0);
-    }
+    window.scrollTo(0, 0);
+  }
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return rect.top + scrollTop
-    }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return rect.top + scrollTop
+  }
+
 })();
